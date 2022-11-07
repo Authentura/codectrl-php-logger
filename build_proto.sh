d9afdfefaa766955ad58d9bd1fce1a0c252164e2
z9fr <z9fr@protonmail.com>
2022-11-07 23:17:20 +0530

    chore: grpc setup and docs

--- build_proto.sh
+++ build_proto.sh
@@ -1,5 +1,1 @@
-protoc --proto_path=codectrl-protobuf-specifications \
-  --php_out=src/protobuf \
-  --grpc_out=src/protobuf \
-  --plugin=protoc-gen-grpc=bins/opt/grpc_php_plugin \ 
-  ./codectrl-protobuf-specifications/cc_service.proto
+protoc --proto_path=protos --php_out=src/protobuf --grpc_out=src/protobuf --plugin=protoc-gen-grpc=bins/opt/grpc_php_plugin ./protos/cc_service.proto
